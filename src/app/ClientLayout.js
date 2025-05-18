7537007b6cfec46600f1a3e943542fa2b08efe91
Adegbola AbdulBaqee <baqeecodes@gmail.com>
2025-05-18 19:32:54 +0100

    Added poppins font

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -3,6 +3,6 @@
 import { useEffect } from "react";
 
 export default function ClientLayout({ children }) {
-    // This component doesn't need to wrap children in AuthProvider since that's already done in layout.js
-    return < > { children } < />;
+  // This component doesn't need to wrap children in AuthProvider since that's already done in layout.js
+  return <> {children} </>;
 }
